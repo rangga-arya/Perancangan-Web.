3ae4c2fc2a7013d63867eec64956520a23c03909
rangga-arya <ranggaarya169@gmail.com>
2026-01-13 02:35:58 +0700

    Add files via upload

---
+++ Praktikum 9/Codingan/config.php
@@ -0,0 +1,11 @@
+<?php
+return [
+  'smtp_host' => 'smtp.gmail.com',
+  'smtp_port' => 587,
+  'smtp_user' => 'ranggaarya169@gmail.com',
+  'smtp_pass' => 'rksklvhygeuyhamh',
+  'from_email' => 'ranggaarya169@gmail.com',
+  'from_name'  => 'Sistem Data Mahasiswa',
+  'to_email'   => 'fadilahakbar143@gmail.com',
+  'to_name'    => 'Penerima'
+];
